1a9695fba3fb43866e975e062a96fb330877accf
Dhxnushh <dhanush2675@gmail.com>
2024-01-31 14:27:39 +0530

    modified:   index.html 	modified:   index.js 	modified:   styles.css

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-function send(){
+function send(event){
+    event.preventDefault();
     var task = document.getElementById("task");
+    let date = new Date();
+    let time = date.toLocaleTimeString();
+    if(task.value===""){
+        return;
+    }
     content = `
-    <li style="font-size:2em; text-align: left;">${task.value}</li>
+    <li style="font-size:2em; text-align: left;"><span>${task.value}</span><span id="time";">${time}</span></li>
     `
     console.log(task)
     document.getElementById("display").innerHTML+=content;
