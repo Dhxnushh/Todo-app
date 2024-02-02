dcee603169e5899c9d5a7eab5b52bea2a11c1a8d
Dhanush.S <132473127+Dhxnushh@users.noreply.github.com>
2024-02-02 20:20:36 +0530

    new file:   __pycache__/app.cpython-312.pyc 	new file:   app.py 	deleted:    index.html 	deleted:    index.js 	renamed:    styles.css -> static/styles.css 	new file:   templates/index.html 	new file:   user.db

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template,request
+from cs50 import SQL
+app = Flask(__name__)
+db = SQL('sqlite:///user.db')
+app.config['SECRET_KEY'] = 'TODO_APP'
+
+
+@app.route("/",methods = ["POST","GET"])
+def todo():
+    if request.method=="POST":
+        task = request.get_json()
+        db.execute('insert into todo(task) values(?)',task)
+    rows = db.execute('select * from todo')
+    l=[]
+    for i in range(len(rows)):
+        l.append(rows[i]['task'])
+    print(l)
+    return render_template("index.html",l=l)
+
+if (__name__)==("__main__"):
+    app.run(debug=True)
